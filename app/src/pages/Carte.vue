84cfb3e577a8dcd2af5ed4fd0ae53b1073f2c1c5
Franky Snow <FrankySnow@users.noreply.github.com>
2020-04-24 01:11:42 +0200

    add Leaflet

--- app/src/pages/Carte.vue
+++ app/src/pages/Carte.vue
@@ -1,11 +1,52 @@
 <template>
-  <q-page class="flex flex-center">
-    Bientôt 🙂
+  <q-page class="flex">
+    <div style="flex:auto">
+      <l-map
+        :zoom="zoom"
+        :center="center"
+        :options="mapOptions"
+        @update:center="centerUpdate"
+        @update:zoom="zoomUpdate"
+      >
+        <l-tile-layer :url="url" />
+      </l-map>
+    </div>
   </q-page>
 </template>
 
 <script>
+import L, { latLng } from 'leaflet'
+import { LMap, LTileLayer } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+
 export default {
   name: 'Carte',
+  components: {
+    LMap,
+    LTileLayer,
+  },
+  data() {
+    return {
+      zoom: 11,
+      center: latLng(46.202, 6.141),
+      url:
+        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      attribution:
+        '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      currentZoom: 11,
+      currentCenter: latLng(46.202, 6.141),
+      mapOptions: {
+        zoomSnap: 1,
+      },
+    }
+  },
+  methods: {
+    zoomUpdate(zoom) {
+      this.currentZoom = zoom
+    },
+    centerUpdate(center) {
+      this.currentCenter = center
+    },
+  },
 }
 </script>
