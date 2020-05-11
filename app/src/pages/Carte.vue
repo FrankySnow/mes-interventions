4fae62c603ac4c437701e7d3b5113ea2e91eb612
Franky Snow <FrankySnow@users.noreply.github.com>
2020-05-12 00:34:52 +0200

    initialize Mapbox with QEnv

--- app/src/pages/Carte.vue
+++ app/src/pages/Carte.vue
@@ -1,11 +1,34 @@
 <template>
   <q-page class="flex flex-center">
-    Bientôt 🙂
+    <div class="absolute-full">
+      <div ref="map" class="fit"></div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN
+const Genève = [6.141, 46.202]
+
 export default {
   name: 'Carte',
+  data() {
+    return {
+      map: null,
+    }
+  },
+  mounted() {
+    this.map = new mapboxgl.Map({
+      container: this.$refs.map,
+      style:
+        'mapbox://styles/frankysnow/ck9yrtmum04rv1ipbjlk6ysvy/draft',
+      center: Genève,
+      zoom: 12,
+    })
+  },
+  methods: {},
 }
 </script>
