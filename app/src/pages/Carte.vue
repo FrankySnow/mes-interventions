11a2e0d375443dc0494b79458c7ba7be1e8767cf
Franky Snow <22062145+FrankySnow@users.noreply.github.com>
2020-09-18 02:48:06 +0200

    ✨ implement search result marker with MapboxMarker

--- app/src/pages/Carte.vue
+++ app/src/pages/Carte.vue
@@ -8,19 +8,22 @@
         :attribution-control="false"
         :zoom="12"
         class="fit"
+        @mb-created="mapInstance => (map = mapInstance)"
       >
         <mapbox-geocoder
           position="top-right"
           :options="{
             bbox: [5.89, 46.11, 6.32, 46.37],
             collapsed: true,
-            marker: {
-              color: 'gold',
-              // draggable: true,
-              // TODO: access marker to implement dragging interaction
-            },
             types: 'address',
           }"
+          :marker="false"
+          @mb-result="onGeocoderResult"
+        />
+        <mapbox-marker
+          :lng-lat="searchResultLngLat"
+          v-if="showSearchResultMarker"
+          color="gold"
         />
         <mapbox-navigation-control
           position="top-right"
@@ -33,6 +36,12 @@
           :trackUserLocation="true"
           :showAccuracyCircle="false"
         />
+        <!-- <q-dialog
+        v-model="bottomDialog"
+        position="bottom"
+        seamless
+      >
+      </q-dialog> -->
       </mapbox-map>
     </div>
   </q-page>
@@ -43,23 +52,40 @@ import {
   MapboxMap,
   MapboxNavigationControl,
   MapboxGeocoder,
+  MapboxMarker,
 } from '@studiometa/vue-mapbox-gl'
 import MapboxGeolocateControl from '@studiometa/vue-mapbox-gl/src/components/MapboxGeolocateControl.vue' // FIXME: component not exported in the build
 import 'mapbox-gl/dist/mapbox-gl.css'
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css'
 
 export default {
-  name: 'Calendrier',
+  name: 'Carte',
   components: {
     MapboxMap,
     MapboxNavigationControl,
     MapboxGeolocateControl,
     MapboxGeocoder,
+    MapboxMarker,
   },
   data() {
     return {
       token: process.env.MAPBOX_ACCESS_TOKEN,
+      map: null,
+      searchResultLngLat: null,
+      showSearchResultMarker: false,
     }
+  },
+  methods: {
+    async onGeocoderResult(e) {
+      this.showSearchResultMarker = true
+      this.searchResultLngLat = e.result.center
+      // Nécessaire pour que flyTo() démarre après l'insertion du Marker
+      await this.$nextTick()
+      this.map.flyTo({
+        center: e.result.center,
+        zoom: 17,
+      })
+    },
   },
 }
 </script>
