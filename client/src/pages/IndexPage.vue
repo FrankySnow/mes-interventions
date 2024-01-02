c43b9a6843b3b693b0c04c77cc13e0c660626c08
Florian <22062145+FrankySnow@users.noreply.github.com>
2024-01-02 22:27:22 +0000

    create basic scaffolding

--- client/src/pages/IndexPage.vue
+++ client/src/pages/IndexPage.vue
@@ -1,42 +1,112 @@
+<script setup lang="ts">
+import { date as QuasarDate, useQuasar } from 'quasar'
+import { reactive, ref } from 'vue'
+
+const $q = useQuasar()
+
+const formValues = reactive({
+  date: QuasarDate.formatDate(Date.now(), 'YYYY/MM/DD'),
+  adresse: 'foo',
+  évènement: 'Incendie',
+  véhicule: 'Tonne',
+  rôle: 'T2',
+  remarques: 'Ct le fuun',
+})
+
+const loading = ref(false)
+const submit = () => {
+  loading.value = true
+  setTimeout(() => {
+    loading.value = false
+    $q.notify(
+      "C'est sauvegardé mais pas pour de vrai 🤣",
+    )
+  }, 2000)
+}
+</script>
+
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    />
+  <q-page class="col q-gutter-md q-pa-md">
+    <q-input
+      v-model="formValues.date"
+      label="Date"
+      outlined
+      mask="date"
+    >
+      <template #before>
+        <q-icon name="event" />
+      </template>
+      <template #append>
+        <q-icon
+          name="edit"
+          class="cursor-pointer"
+        >
+          <q-popup-proxy
+            ref="qDateProxy"
+            transition-show="scale"
+            transition-hide="scale"
+          >
+            <q-date
+              v-model="formValues.date"
+            />
+          </q-popup-proxy>
+        </q-icon>
+      </template>
+    </q-input>
+    <q-input
+      v-model="formValues.adresse"
+      label="Adresse"
+      outlined
+    >
+      <template #before>
+        <q-icon name="place" />
+      </template>
+    </q-input>
+    <q-input
+      v-model="formValues.évènement"
+      label="Évènement"
+      outlined
+    >
+      <template #before>
+        <q-icon name="fireplace" />
+      </template>
+    </q-input>
+    <q-input
+      v-model="formValues.véhicule"
+      label="Véhicule"
+      outlined
+    >
+      <template #before>
+        <q-icon name="local_shipping" />
+      </template>
+    </q-input>
+    <q-input
+      v-model="formValues.rôle"
+      label="Rôle"
+      outlined
+    >
+      <template #before>
+        <q-icon name="person" />
+      </template>
+    </q-input>
+    <q-input
+      v-model="formValues.remarques"
+      type="textarea"
+      label="Remarques"
+      outlined
+    >
+      <template #before>
+        <q-icon name="notes" />
+      </template>
+    </q-input>
+    <div class="row">
+      <q-btn
+        :loading="loading"
+        color="primary"
+        label="sauvegarder"
+        class="full-width"
+        @click="submit"
+      />
+    </div>
   </q-page>
 </template>
-
-<script setup lang="ts">
-import { Todo, Meta } from 'components/models'
-import ExampleComponent from 'components/ExampleComponent.vue'
-import { ref } from 'vue'
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1',
-  },
-  {
-    id: 2,
-    content: 'ct2',
-  },
-  {
-    id: 3,
-    content: 'ct3',
-  },
-  {
-    id: 4,
-    content: 'ct4',
-  },
-  {
-    id: 5,
-    content: 'ct5',
-  },
-])
-const meta = ref<Meta>({
-  totalCount: 1200,
-})
-</script>
--- client/src/router/routes.ts
+++ client/src/router/routes.ts
@@ -1,10 +1,12 @@
 import { RouteRecordRaw } from 'vue-router'
+import MainLayout from 'layouts/MainLayout.vue'
+import IndexPage from 'pages/IndexPage.vue'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [{ path: '', component: () => import('pages/IndexPage.vue') }],
+    component: MainLayout,
+    children: [{ path: '', component: IndexPage }],
   },
 
   // Always leave this as last one,
